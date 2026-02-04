819d7caf20ef31c1deafd5d7b0aa4d7b09427282
Amodio <jacques@boscq.fr>
2026-02-04 23:40:20 +0100

    Several improvements to test.py:   - explanation to using mfa_secret   - detail of what is printed with some indentation   - history transactions limited to last december   - fix missing price for the dry run order: Error placing order: Bad Request : Wrong order price Reference code: 1062   - fix cancellation of dry_run order leading to an error: Traceback (most recent call last):   File "/home/da/./test.py", line 104, in <module>     cancel = ft_accounts.cancel_order(order_conf["result"]["order_id"])                                       ~~~~~~~~~~~~~~~~~~~~^^^^^^^^^^^^ KeyError: 'order_id'   - dry_run/preview option was not setting the price despite the preceding text, it was using a MARKET type of price, leading to an error if not executed in market hours: Option Market orders are not accepted outside of regular market hour.  Please enter a limit order.

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
+#!/usr/bin/python3
+
 from firstrade import account, order, symbols
+import json
 
 # Create a session
-ft_ss = account.FTSession(username="", password="", email="", profile_path="")
+# mfa_secret is the secret key to generate TOTP (not the backup code), see:
+# https://help.firstrade.info/en/articles/9260184-two-factor-authentication-2fa
+ft_ss = account.FTSession(username="", password="", mfa_secret="")
+#ft_ss = account.FTSession(username="", password="", email="", profile_path="")
 need_code = ft_ss.login()
 if need_code:
     code = input("Please enter the pin sent to your email/phone: ")
@@ -13,64 +19,64 @@ if len(ft_accounts.account_numbers) < 1:
     raise Exception("No accounts found or an error occured exiting...")
 
 # Print ALL account data
-print(ft_accounts.all_accounts)
+print(f"Account data: {json.dumps(ft_accounts.all_accounts, indent=2)}")
 
 # Print 1st account number.
-print(ft_accounts.account_numbers[0])
+print(f"1st account number: {ft_accounts.account_numbers[0]}")
 
 # Print ALL accounts market values.
-print(ft_accounts.account_balances)
+print(f"Account(s) current balance(s): {ft_accounts.account_balances}")
 
 # Get quote for INTC
 quote = symbols.SymbolQuote(ft_ss, ft_accounts.account_numbers[0], "INTC")
-print(f"Symbol: {quote.symbol}")
-print(f"Tick: {quote.tick}")
-print(f"Exchange: {quote.exchange}")
-print(f"Bid: {quote.bid}")
-print(f"Ask: {quote.ask}")
-print(f"Last: {quote.last}")
-print(f"Bid Size: {quote.bid_size}")
-print(f"Ask Size: {quote.ask_size}")
-print(f"Last Size: {quote.last_size}")
-print(f"Bid MMID: {quote.bid_mmid}")
-print(f"Ask MMID: {quote.ask_mmid}")
-print(f"Last MMID: {quote.last_mmid}")
-print(f"Change: {quote.change}")
-print(f"High: {quote.high}")
-print(f"Low: {quote.low}")
-print(f"Change Color: {quote.change_color}")
-print(f"Volume: {quote.volume}")
-print(f"Quote Time: {quote.quote_time}")
-print(f"Last Trade Time: {quote.last_trade_time}")
-print(f"Real Time: {quote.realtime}")
-print(f"Fractional: {quote.is_fractional}")
-print(f"Company Name: {quote.company_name}")
+print("Quote for INTC:")
+print(f"\tSymbol: {quote.symbol}")
+print(f"\tTick: {quote.tick}")
+print(f"\tExchange: {quote.exchange}")
+print(f"\tBid: {quote.bid}")
+print(f"\tAsk: {quote.ask}")
+print(f"\tLast: {quote.last}")
+print(f"\tBid Size: {quote.bid_size}")
+print(f"\tAsk Size: {quote.ask_size}")
+print(f"\tLast Size: {quote.last_size}")
+print(f"\tBid MMID: {quote.bid_mmid}")
+print(f"\tAsk MMID: {quote.ask_mmid}")
+print(f"\tLast MMID: {quote.last_mmid}")
+print(f"\tChange: {quote.change}")
+print(f"\tHigh: {quote.high}")
+print(f"\tLow: {quote.low}")
+print(f"\tChange Color: {quote.change_color}")
+print(f"\tVolume: {quote.volume}")
+print(f"\tQuote Time: {quote.quote_time}")
+print(f"\tLast Trade Time: {quote.last_trade_time}")
+print(f"\tReal Time: {quote.realtime}")
+print(f"\tFractional: {quote.is_fractional}")
+print(f"\tCompany Name: {quote.company_name}")
 
 # Get positions and print them out for an account.
 positions = ft_accounts.get_positions(account=ft_accounts.account_numbers[0])
-print(positions)
+print(f"Current positions held in account {ft_accounts.account_numbers[0]}: {json.dumps(positions, indent=2)}")
+print(f"Current positions (summed up) held in account {ft_accounts.account_numbers[0]}:")
 for item in positions["items"]:
-    print(
-        f"Quantity {item['quantity']} of security {item['symbol']} held in account {ft_accounts.account_numbers[0]}",
-    )
+    print(f"\t{item['quantity']}\tof security {item['symbol']}.")
 
-# Get account history (past 200)
+# Get account history for a custom date range
 history = ft_accounts.get_account_history(
     account=ft_accounts.account_numbers[0],
     date_range="cust",
-    custom_range=["2024-01-01", "2024-06-30"],
+    custom_range=["2025-12-01", "2025-12-31"],
 )
 
-for item in history["items"]:
-    print(
-        f"Transaction: {item['symbol']} on {item['report_date']} for {item['amount']}.",
-    )
-
+print(f"Transaction history (December 2025) for account #{ft_accounts.account_numbers[0]}: {json.dumps(history, indent=2)}")
+if len(history["items"]) > 0:
+    print("Transaction history (summed up) for December 2025:")
+    for item in history["items"]:
+        print(f"\t{item['report_date']}: {item['amount']}$\tof {item['symbol']}")
 
 # Create an order object.
 ft_order = order.Order(ft_ss)
 
-# Place dry run order and print out order confirmation data.
+# Place a dry run order and print out order confirmation data.
 order_conf = ft_order.place_order(
     ft_accounts.account_numbers[0],
     symbol="INTC",
@@ -78,37 +84,38 @@ order_conf = ft_order.place_order(
     order_type=order.OrderType.BUY,
     duration=order.Duration.DAY,
     quantity=1,
+    price=3.37,
     dry_run=True,
 )
-
-print(order_conf)
+print(f"Preview of an order to buy 1 share of INTC: {json.dumps(order_conf, indent=2)}")
 
 if order_conf.get("error"):
     print(f"Error placing order: {order_conf['error']} : {order_conf['message']}")
 elif "order_id" not in order_conf["result"]:
-    print("Dry run complete.")
-    print(order_conf["result"])
+    print(f"Dry run complete!")
 else:
-    print("Order placed successfully.")
-    print(f"Order ID: {order_conf['result']['order_id']}.")
-    print(f"Order State: {order_conf['result']['state']}.")
+    print("Order placed successfully!")
+    print(f"\tOrder ID: {order_conf['result']['order_id']}.")
+    print(f"\tOrder State: {order_conf['result']['state']}.")
 
-# Cancel placed order
-if not order_conf.get("error"):
+# Cancel placed order (on success and if it was not a dry_run)
+if not order_conf.get("error") and "order_id" in order_conf["result"]:
     cancel = ft_accounts.cancel_order(order_conf["result"]["order_id"])
     if cancel["result"]["result"] == "success":
-        print("Order cancelled successfully.")
-        print(cancel)
+        print(f"Order cancelled successfully: {cancel}.")
+    else:
+        print(f"Cannot cancel order: {cancel}.")
 
 # Check orders
 recent_orders = ft_accounts.get_orders(ft_accounts.account_numbers[0])
-print(recent_orders)
+print(f"Recent orders: {json.dumps(recent_orders, indent=2)}")
 
-# Get option dates
+# Get option dates for a symbol
 option_first = symbols.OptionQuote(ft_ss, "INTC")
+print("Option expiration dates for INTC:")
 for item in option_first.option_dates["items"]:
     print(
-        f"Expiration Date: {item['exp_date']} Days Left: {item['day_left']} Expiration Type: {item['exp_type']}",
+        f"\tExpiration Date: {item['exp_date']} Days Left: {item['day_left']} Expiration Type: {item['exp_type']}",
     )
 
 # Get option quote
@@ -116,32 +123,35 @@ option_quote = option_first.get_option_quote(
     "INTC",
     option_first.option_dates["items"][0]["exp_date"],
 )
-print(option_quote)
+limited_option_quote = {
+    **option_quote,
+    "items": option_quote["items"][:2]
+}
+print(f"Option quote for INTC (limited to the first two items): {json.dumps(limited_option_quote, indent=2)}")
 
 # Get option greeks
 option_greeks = option_first.get_greek_options(
     "INTC",
     option_first.option_dates["items"][0]["exp_date"],
 )
-print(option_greeks)
-
-print(
-    f"Placing dry option order for {option_quote['items'][0]['opt_symbol']} with a price of {option_quote['items'][0]['ask']}.",
-)
-print("Symbol readable ticker 'INTC'")
+limited_option_greeks = {
+    **option_greeks,
+    "chains": option_greeks["chains"][:2]
+}
+print(f"Option greeks at {option_first.option_dates["items"][0]["exp_date"]} for INTC (limited to the first two chains): {json.dumps(limited_option_greeks, indent=2)}")
 
 # Place dry option order
 option_order = ft_order.place_option_order(
     account=ft_accounts.account_numbers[0],
     option_symbol=option_quote["items"][0]["opt_symbol"],
     order_type=order.OrderType.BUY_OPTION,
-    price_type=order.PriceType.MARKET,
+    price_type=order.PriceType.LIMIT,
     duration=order.Duration.DAY,
+    price=0.01,
     contracts=1,
     dry_run=True,
 )
+print(f"Preview of an option order for {option_quote['items'][0]['opt_symbol']}: {json.dumps(option_order, indent=2)}")
 
-print(option_order)
-
-# Delete cookies
-ft_ss.delete_cookies()
+# Delete the session cookie
+#ft_ss.delete_cookies()
