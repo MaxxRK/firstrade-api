f0d892b83e085c346e56dc0c2d2f9529eab51432
MaxxRK <drgullett89@outlook.com>
2026-02-05 14:44:05 -0600

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = pathlib.Path("README.md").read_text()
 
 setuptools.setup(
     name="firstrade",
-    version="0.0.37",
+    version="0.0.38",
     author="MaxxRK",
     author_email="maxxrk@pm.me",
     description="An unofficial API for Firstrade",
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     license="MIT",
     url="https://github.com/MaxxRK/firstrade-api",
-    download_url="https://github.com/MaxxRK/firstrade-api/archive/refs/tags/0037.tar.gz",
+    download_url="https://github.com/MaxxRK/firstrade-api/archive/refs/tags/0038.tar.gz",
     keywords=["FIRSTRADE", "API"],
     install_requires=["requests", "pyotp"],
     packages=["firstrade"],
